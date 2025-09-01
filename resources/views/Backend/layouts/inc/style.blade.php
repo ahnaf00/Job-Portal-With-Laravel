5d20be9f1b6a895cd130016b3bfa026213dc47c5
Ahnaf Anan <aanana0171@gmail.com>
2025-09-01 21:35:16 +0600

    templated admin dashboard

---
+++ resources/views/Backend/layouts/inc/style.blade.php
@@ -0,0 +1,9 @@
+<!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{ 'Backend/assets' }}/css/argon-dashboard.css?v=2.1.0" rel="stylesheet" />
